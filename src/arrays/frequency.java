27488d9b39cb9bed13c610a8b868f2069d6c19ba
work-mayankraj <me.rajmayank@gmail.com>
2026-03-21 23:49:43 +0530

    Frequency of ech uniue element

---
+++ src/arrays/frequency.java
@@ -0,0 +1,49 @@
+package arrays;
+import java.util.*;
+
+public class frequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        int[] freq = new int[n];
+
+        // Initialize freq array with -1
+        for(int i = 0; i < n; i++) {
+            freq[i] = -1;
+        }
+
+        // array input
+        System.out.print("Enter array element: ");
+        for(int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // frequency logic
+        for(int i = 0; i < n; i++) {
+            if(freq[i] == -1) {
+                int count = 1;
+
+                for(int j = i + 1; j < n; j++) {
+                    if(nums[i] == nums[j]) {
+                        count++;
+                        freq[j] = 0;   // mark duplicate
+                    }
+                }
+
+                freq[i] = count;  // assign after loop
+            }
+        } 
+
+        // print result
+        System.out.println("\nElement  Frequency");
+        for(int i = 0; i < n; i++) {
+            if(freq[i] != 0) {
+                System.out.println(nums[i] + "        " + freq[i]);
+            }
+        }
+    }
+}
