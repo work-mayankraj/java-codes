9218f798521c6542fb8a0f44316a64df21aa520a
work-mayankraj <me.rajmayank@gmail.com>
2026-04-11 23:53:38 +0530

    Armstrong number

---
+++ src/arrays/MoveZeroToEnd.java
@@ -0,0 +1,32 @@
+package arrays;
+import java.util.*;
+public class MoveZeroToEnd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a number :");
+		int num = sc.nextInt();
+		
+		int[] arr = new int[num];
+		System.out.print("Enter elements :");
+		for(int i=0; i<num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int k=0;
+		int[] arr2 = new int[num];
+		for(int j=0; j<num; j++) {
+			if(arr[j]!=0) {
+				arr2[k] = arr[j];
+				k++;
+			}
+		}
+		System.out.print("Array after moving all zeros at end :");
+		for(int j=0; j< num; j++) {
+			System.out.print(arr2[j] + " ");
+		}
+	
+	}
+
+}
