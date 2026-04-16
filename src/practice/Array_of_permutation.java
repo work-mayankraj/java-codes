c4369300ba857565e617748695f5ecb867ce0fe2
work-mayankraj <me.rajmayank@gmail.com>
2026-04-16 21:26:59 +0530

    Insert elements at given indice

---
+++ src/practice/Array_of_permutation.java
@@ -0,0 +1,25 @@
+package Practice;
+import java.util.*;
+public class Array_of_permutation {
+/*“Given an array representing a permutation, create a new array such that each element
+at index i is replaced by the value at index arr[i] of the original array.” */ 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Enter array size:");
+		int size = sc.nextInt();
+		
+		int[] arr = new int[size];
+		for(int i = 0; i<size; i++) {
+			arr[i] =  sc.nextInt();
+			if(arr[i]>=size) System.out.println("Invalid range");
+		}
+		int[] ans = new int[size];
+		for(int i=0; i<size; i++) {
+			ans[i] = arr[arr[i]];
+		}
+		
+		System.out.println(Arrays.toString(ans));
+	}
+}
