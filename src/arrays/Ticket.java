aa2634247e74654e74a11cd43ce42b5c7812b2ab
work-mayankraj <me.rajmayank@gmail.com>
2026-04-22 23:28:31 +0530

    Ticket

---
+++ src/arrays/Ticket.java
@@ -0,0 +1,22 @@
+package arrays;
+import java.util.*;
+public class Ticket {
+	    public static void main(String[] args) {
+	        int[] nums = {1, 2, 3, 1};
+
+	        boolean result = containsDuplicate(nums);
+	        System.out.println("Contains Duplicate: " + result);
+	    }
+
+	    public static boolean containsDuplicate(int[] nums) {
+	        HashSet<Integer> set = new HashSet<>();
+
+	        for (int num : nums) {
+	            if (set.contains(num)) return true;
+	            set.add(num);
+	        }
+
+	        return false;
+	    }
+	}
+}
