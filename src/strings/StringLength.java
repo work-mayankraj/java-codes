c1bf620d5f3a853cba325fa3185a09e33178b4d9
work-mayankraj <me.rajmayank@gmail.com>
2026-04-24 23:05:25 +0530

    Counting length of array

---
+++ src/strings/StringLength.java
@@ -0,0 +1,15 @@
+package strings;
+import java.util.*;
+public class StringLength {
+	
+	public static void main(String[] args) {
+		String str = "Hello";
+		
+		int count = 0;
+// toCharArray()
+		for(char c: str.toCharArray()) {
+			count++;
+		}
+		System.out.print("Length of string :"+count);
+	}
+}
