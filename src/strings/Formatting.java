c3adb3bbdc0f665fc38a5d7ee6b60c818f1d3c81
work-mayankraj <me.rajmayank@gmail.com>
2026-05-02 00:00:24 +0530

    Formatted String

---
+++ src/strings/Formatting.java
@@ -0,0 +1,21 @@
+package strings;
+import java.util.*;
+
+public class Formatting {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter int value: ");
+        long l = input.nextLong();
+
+        System.out.println("Enter double value: ");
+        double db = input.nextDouble();
+
+        // Pass values inside format()
+        System.out.println(String.format("Formatted integer: %,d", l));
+        System.out.println(String.format("Formatted double: %.2f", db));
+
+        input.close();
+    }
+}
