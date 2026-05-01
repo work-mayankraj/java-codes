a3513f5fd76e6f2c87b0417887b7a003cc02b29a
work-mayankraj <me.rajmayank@gmail.com>
2026-05-01 23:32:04 +0530

    String formatting in date

---
+++ src/strings/Date.java
@@ -0,0 +1,23 @@
+package strings;
+
+import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+
+	public static void main(String[] args) {
+
+		// Get current date
+		LocalDate today = LocalDate.now();
+
+		// Create formatter
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
+
+		// Format date into string
+		String formattedDate = today.format(format);
+
+		// Print result
+		System.out.println("Today's date: " + formattedDate);
+	}
+}
