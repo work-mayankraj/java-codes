2c0da47e4d6edcb3dcc23c34b1379a15a45ac040
work-mayankraj <me.rajmayank@gmail.com>
2026-05-04 12:41:04 +0530

    Reverse string without using any other variable

---
+++ src/strings/ReverseString.java
@@ -0,0 +1,15 @@
+package strings;
+import java.util.*;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a string: ");
+		String str = sc.nextLine();
+		
+		for(int i = str.length()-1; i>=0; i--) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
